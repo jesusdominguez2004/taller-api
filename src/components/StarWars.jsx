358b848e21e79e0b2e88ba1eaf620c20a44173b9
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2023-05-30 17:10:02 -0500

    4. Elementos HTML y funciones

--- src/components/StarWars.jsx
+++ src/components/StarWars.jsx
@@ -5,11 +5,29 @@ const StarWars = () => {
     // Variables hooks
     const [personajes, setPersonajes] = useState([]);
     const [paginacion, setPaginacion] = useState(0);
-    
 
+    // Funciones
+    const traer = () => {
+        setPaginacion(paginacion + 1);
+    }
+
+    const siguiente = () => {
+        setPaginacion(paginacion + 1);
+    }
+
+    const atras = () => {
+        setPaginacion(paginacion - 1);
+    }
 
     return (
-        <div>StarWars</div>
+        <div>
+            <h1>Solicitud API Star Wars®</h1>
+            <h2>Buenas Prácticas de Desarrollo de Software</h2>
+            <h2>© Jesús Domínguez</h2>
+            <button onClick={traer}>Personajes</button>
+            <button onClick={siguiente}>Siguiente</button>
+            <button onClick={atras}>Atrás</button>
+        </div>
     )
 }
 
