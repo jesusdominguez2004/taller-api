f84a5f614df618b0279b9e7014f17b1583266367
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2023-05-30 17:04:36 -0500

    3. Variables hooks

--- src/components/StarWars.jsx
+++ src/components/StarWars.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
 
 const StarWars = () => {
+    // Variables hooks
+    const [personajes, setPersonajes] = useState([]);
+    const [paginacion, setPaginacion] = useState(0);
+    
+
+
     return (
         <div>StarWars</div>
     )
